3f8e989b356e03207b8ba329bf371dedfcde5e4b
hailindai <hailin_dai@163.com>
2017-03-15 11:20:43 +0800

    update demo choose video file

--- app/src/main/java/com/dreamguard/sample/MainActivity.java
+++ app/src/main/java/com/dreamguard/sample/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dreamguard.sample;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Surface;
@@ -19,34 +22,41 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
 
     private GPUVideoView videoView;
+
+    private SurfaceTexture mSurfaceTexture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        videoView = (GPUVideoView)findViewById(R.id.videoView);
+        videoView = (GPUVideoView) findViewById(R.id.videoView);
 
         initGPUVideoView();
 
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("video/*");
+        startActivityForResult(intent, 1);
+
     }
 
-    public void initGPUVideoView()
-    {
+    public void initGPUVideoView() {
         videoView.init(new IVideoSurface() {
             @Override
             public void onCreated(SurfaceTexture surfaceTexture) {
-                initPlayer(surfaceTexture);
+                mSurfaceTexture = surfaceTexture;
             }
         });
 
         videoView.setFilter(new GPUVideoSobelEdgeDetection());
     }
-    public void initPlayer(SurfaceTexture surfaceTexture){
+
+    public void initPlayer(SurfaceTexture surfaceTexture, String path) {
 
         mediaPlayer = new MediaPlayer();
 
         try {
-            mediaPlayer.setDataSource(this, Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.mv));
-//            mediaPlayer.setDataSource(Environment.getExternalStorageDirectory().getAbsolutePath()+"/s.mp4");
+            mediaPlayer.setDataSource(path);
             Surface s = new Surface(surfaceTexture);
             mediaPlayer.setSurface(s);
             s.release();
@@ -64,9 +74,20 @@ public class MainActivity extends AppCompatActivity {
         stopPlayer();
     }
 
-    public void stopPlayer(){
-        if(mediaPlayer != null && mediaPlayer.isPlaying()){
+    public void stopPlayer() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
             mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+            Uri selectedVideo = data.getData();
+            String videoPath = selectedVideo.getPath();
+            initPlayer(mSurfaceTexture, videoPath);
         }
     }
 }
